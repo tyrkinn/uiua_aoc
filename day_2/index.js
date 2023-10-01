479502fb1a4ca93daa9cd3f9d6b84b33aa55a6dc
Nikita Tyrkin <tyrkinn@pm.me>
2023-10-01 18:00:06 +0300

    feat: update js solution

--- day_2/index.js
+++ day_2/index.js
@@ -1,31 +1,49 @@
 const { readFileSync } = require('fs')
 const data = readFileSync('./input.txt')
 
+Object.defineProperty(Array.prototype, 'chunk', {
+  value: function(size) {
+    const clone = [...this]
+    const chunks = []
+    while(clone.length) {
+      chunks.push(clone.splice(0, size))
+    }
+    return chunks;
+  }
+})
+
+Object.defineProperty(Array.prototype, 'duplicate', {
+  value: function() {
+    return [[...this], [...this]];
+  }
+})
+
+
+Object.defineProperty(Array.prototype, 'fork', {
+  value: function(mapper1, mapper2) {
+    if (this.length !== 2) return this
+    const [f, s] = this;
+    return [mapper1(f), mapper2(s)];
+  }
+})
+
+const sumWith = valueMapper => list => list.reduce((acc, v) => acc + valueMapper(v), 0)
+
 const lu = ['ABC', 'XYZ']
-const lines = data
-  .toString()
-  .trim()
-  .split('\n')
-  .map(
-    l => l
-      .split(' ')
-      .map((c, i) => lu[i].indexOf(c)))
 
 const mod = (n, p) => n - p * Math.floor(n / p);
 
-const part1 = lines.reduce(
-  (acc, [o, m]) =>
-    acc + m + 1 + mod(m - o + 1, 3) * 3,
-  0
-)
+const lines = data
+  .toString()
+  .replaceAll('\n', ' ')
+  .split(' ')
+  .map((c, i) => lu[i % 2].indexOf(c))
+  .chunk(2)
+  .duplicate()
+  .fork(
+    sumWith(([op, my]) => my + 1 + mod(my - op + 1, 3) * 3),
+    sumWith(([op, my]) => my * 3 + mod(my + op - 1, 3) + 1),
+  )
 
-const part2 = lines.reduce(
-  (acc, [o, m]) => 
-    acc + m * 3 + mod(m + o - 1, 3) + 1,
-   0
-)
-
-
-console.log(part1)
-console.log(part2)
+console.log(lines)
 
